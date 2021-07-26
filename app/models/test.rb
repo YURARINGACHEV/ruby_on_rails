3dc72e59fd499c518a3ad579c4bce373ef5eff5e
YOUR NAME <YOUR EMAIL ADDRESS>
2021-07-26 16:08:10 +1000

    Пробная версия,понять в том напралении думаю

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,14 @@
+# Создайте метод класса в модели Test, который возвращает отсортированный 
+# по убыванию массив названий всех Тестов у которых Категория называется
+#  определённым образом (название категории передается в метод в качестве аргумента).
+
+
+
 class Test < ApplicationRecord
+
+  def self.order_mas_title(title)
+    # Test.joins('join categories on categories.id = tests.category_id').where(categories: { title: 'Rails' }).order(title: :DESC).pluck('tests.title')
+    Category.joins(:tests).where("title = ?", title).order(id: :DESC)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# # Создайте набор начальных данных для всех существующих моделей в файле db/seeds.rb
+# Создайте метод класса в модели Test, который возвращает отсортированный по убыванию
+#  массив названий всех Тестов у которых Категория называется определённым образом 
+#  (название категории передается в метод в качестве аргумента).
+
+tests = Test.create(title: "Rails",
+                    level: "2")
+tests = Test.create(title: "Rails",
+                    level: "3")
+tests = Test.create(title: "Ruby",
+                    level: "2")
+
+tests = Test.create(title: "SQL",
+                    level: "3")
+
+
+categories = Category.create(title: "Rails.Интерфейс запросов Active Record")
+categories = Category.create(title: "Rails")
+
+users = User.create(name: "Gerrard")
+
+answers = Answer.create(correct: true)
+
+questions = Question.create(body: "Как вывести список всех Тестов,которые проходит или когда-либо проходил Пользователь")
