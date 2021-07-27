09f2f6b4438e1931e834f694f374c8b7bc69d3cd
YOUR NAME <YOUR EMAIL ADDRESS>
2021-07-27 11:34:50 +1000

    добавил фильтр по user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 # которые проходит или когда-либо проходил Пользователь на этом уровне сложности
 class User < ApplicationRecord
   def show_list_test(level)
-    Test.joins('join test_users on tests.id = test_users.test_id').where(tests: { level: level })
+    Test.joins('join test_users on tests.id = test_users.test_id').where(tests: { level: level }).where(test_users: {user_id: self.id})
 
   end
 
