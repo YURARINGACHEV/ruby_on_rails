e56dd749f5dd30e08cf247b4aadf13eed061e155
YOUR NAME <YOUR EMAIL ADDRESS>
2021-08-01 22:55:18 +1000

    Рефакторинг User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   has_many :tests, through: :test_users
 
+  def show_list_test(level)
+    tests.where(level: level)
+  end
+
   # def show_list_test_1(level)
   #   Test.joins('join test_users on tests.id = test_users.test_id').where(tests: { level: level }).where(test_users: {user_id: self.id})
   # end
