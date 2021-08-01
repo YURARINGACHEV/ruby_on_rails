7652e7e5f432b05e62b841d95d46d2999cafb908
YOUR NAME <YOUR EMAIL ADDRESS>
2021-08-01 22:54:11 +1000

    Изменил user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   has_many :test_users
 
-  has_many :tests, through: :test_users 
+  has_many :tests, through: :test_users
 
   # def show_list_test_1(level)
   #   Test.joins('join test_users on tests.id = test_users.test_id').where(tests: { level: level }).where(test_users: {user_id: self.id})
   # end
 
 end
+
