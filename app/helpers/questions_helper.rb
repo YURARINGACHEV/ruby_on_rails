6b206c37a8c94765ba74711aec7dea7913dc1e33
YOUR NAME <YOUR EMAIL ADDRESS>
2021-08-10 17:09:02 +1000

    Доработал добавил _form.html.erb

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,17 +1,12 @@
 module QuestionsHelper
 
-# Для заголовка формы создания и редактирования вопроса. Это должен быть 
-# один метод question_header который в форме создания вопроса будет выводить 
-# Create New <Test Title> Question, а в форме редактирования Edit <Test Title> Question
-# ну, в этом и есть смысл метода, чтобы понять как проверить)
-  def question_header(test_title)
-    if @questions.present?
-      "Create New <#{test_title}> Question"
+  def question_header(question)
+    if question.new_record?
+      "Create New <#{question.test.title}> Question"
     else 
-      "Edit <#{test_title}> Question"
+      "Edit <#{question.test.title}> Question"
     end
         
-    
   end
 
 end
