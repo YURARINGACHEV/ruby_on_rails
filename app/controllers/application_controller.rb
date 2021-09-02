1b70d18379c3401af9dba52ac3892700a59fa396
YOUR NAME <YOUR EMAIL ADDRESS>
2021-09-03 09:48:27 +1000

    изменил метод after_sign_in_path_for(resource)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
 
 	def after_sign_in_path_for(resource)
-    if current_user.is_a?(Admin)
+    if resource.is_a?(Admin)
       admin_tests_path
     else
     	tests_path
