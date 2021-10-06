47f70e382e7d46df01eb35e8536485bc45d114b7
YOUR NAME <YOUR EMAIL ADDRESS>
2021-10-06 23:59:51 +1000

    17

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,6 +2,6 @@ class FormMailer < ApplicationMailer
 
   def form_email(user)
     @name = user.name
-    mail (to: 'yura.ryngachev@gmail.com', subject: "Message from #{@name}")
+    mail(to: 'yura.ryngachev@gmail.com', subject: "Message from #{@name}")
   end
 end
