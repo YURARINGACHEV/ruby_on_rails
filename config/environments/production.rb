934a5749eb0e2c715fa2758527dcd03bd62daa1f
YOUR NAME <YOUR EMAIL ADDRESS>
2021-10-07 16:35:00 +1000

    17 authentication:       'plain'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
     enable_starttls_auto: true }
 
   
